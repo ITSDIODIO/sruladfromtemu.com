6a47132ac5fb0cfa729b0398858f1244a36e1b79
TDudauri <TDudauri1@seu.edu.ge>
2025-01-15 02:36:01 +0400

    movrchi

--- script.js
+++ script.js
@@ -28,4 +28,97 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+  //cookies
+  document.addEventListener("DOMContentLoaded", function () {
+    const cookiesBox = document.querySelector(".cookies");
+    const acceptBtn = document.querySelector("#acceptBtn");
+    const declineBtn = document.querySelector("#declineBtn");
+
+    
+    if (!localStorage.getItem("cookiesAccepted")) {
+        cookiesBox.classList.add("show");
+    }
+
+   
+    document.querySelector('[onclick="changeLanguage(\'ka\')"]').addEventListener('click', function() {
+      changeLanguage('ka');
+  });
   
+  document.querySelector('[onclick="changeLanguage(\'en\')"]').addEventListener('click', function() {
+      changeLanguage('en');
+  });
+  
+
+    
+    declineBtn.addEventListener("click", function () {
+        cookiesBox.classList.remove("show");
+        
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const movieList = document.querySelector(".movie-list");
+  const loadMoreButton = document.querySelector("#loadMore");
+
+  let visibleMovies = 6; 
+  const totalMovies = movieList.children.length; 
+
+  
+  function updateMovies() {
+    for (let i = 0; i < totalMovies; i++) {
+      if (i < visibleMovies) {
+        movieList.children[i].style.display = "block";
+      } else {
+        movieList.children[i].style.display = "none";
+      }
+    }
+  }
+
+  updateMovies();
+
+  
+  loadMoreButton.addEventListener("click", function () {
+    if (visibleMovies < totalMovies) {
+      visibleMovies += 1; 
+      updateMovies();
+    }
+   
+    if (visibleMovies >= totalMovies) {
+      loadMoreButton.style.display = "none";
+    }
+  });
+});
+
+//ვითომ ენგლიშ
+document.addEventListener("DOMContentLoaded", function() {
+  const translations = {
+      "en": {
+          "signin-title": "Sign In",
+          "username-label": "Username or Email",
+          "password-label": "Password",
+          "signin-btn": "Log In",
+          "remember-label": "Remember me"
+      },
+      "ka": {
+          "signin-title": "ავტორიზაცია",
+          "username-label": "მომხმარებილის სახელი ან ემაილი",
+          "password-label": "პაროლი",
+          "signin-btn": "შესვლა",
+          "remember-label": "ჩემი დამახსოვრება"
+      }
+  };
+
+  function changeLanguage(language) {
+      const langData = translations[language];
+
+      document.getElementById("signin-title").textContent = langData["signin-title"];
+      document.getElementById("username-label").textContent = langData["username-label"];
+      document.getElementById("password-label").textContent = langData["password-label"];
+      document.getElementById("signin-btn").textContent = langData["signin-btn"];
+      document.getElementById("remember-label").textContent = langData["remember-label"];
+  }
+
+ 
+  changeLanguage('ka');
+});
